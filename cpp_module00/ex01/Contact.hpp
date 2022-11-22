8ec257dd2a9be9ff45e72c1033c8cf7b2ceb5f32
Hassan Khalil <hkhalil@e2r1p4.1337.ma>
2022-11-22 16:54:11 +0100

    changed Contact class to private

--- cpp_module00/ex01/Contact.hpp
+++ cpp_module00/ex01/Contact.hpp
@@ -2,11 +2,13 @@
 #define  CONTACT_H
 
 class Contact {
-    public:
     std::string first_name;
     std::string last_name;
     std::string nickname;
     std::string phone_number;
     std::string darkest_secret;
+    public: 
+    void  fill_field(int index);
+    std::string  get_field(int index);
 };
 #endif
--- cpp_module00/ex01/main.cpp
+++ cpp_module00/ex01/main.cpp
@@ -2,6 +2,34 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 
+ void  Contact::fill_field(int index)
+ {
+	if (index == 0)
+		std::cin >>this->first_name;
+	else if (index == 1)
+		std::cin >>this->last_name;
+	else if (index == 2)
+		std::cin >>this->nickname;
+	else if (index == 3)
+		std::cin >>this->phone_number;
+	else if (index == 4)
+		std::cin >>this->darkest_secret;
+ }
+
+std::string  Contact::get_field(int index)
+{
+	if (index == 0)
+		return this->first_name;
+	else if (index == 1)
+		return this->last_name;
+	else if (index == 2)
+		return this->nickname;
+	else if (index == 3)
+		return this->phone_number;
+	else
+		return this->darkest_secret;
+}
+
 PhoneBook::PhoneBook(){
 	this->index = 0;
 	this->n = 0;
@@ -11,15 +39,15 @@ void PhoneBook::ADD(){
 	if (this->n < 8)
 		this->n++;
 	std::cout << "first name : ";
-	std::cin >>this->contacts[index].first_name;
+	this->contacts[index].fill_field(0);
 	std::cout << "last name : ";
-	std::cin >>this->contacts[index].last_name;
+	this->contacts[index].fill_field(1);
 	std::cout << "nickname : ";
-	std::cin >>this->contacts[index].nickname;
+	this->contacts[index].fill_field(2);
 	std::cout << "phone number : ";
-	std::cin >>this->contacts[index].phone_number;
+	this->contacts[index].fill_field(3);
 	std::cout << "darkest secret : ";
-	std::cin >>this->contacts[index].darkest_secret;
+	this->contacts[index].fill_field(4);
 	this->index++;
 	if (this->index == 8)
 		this->index = 0;
@@ -38,16 +66,16 @@ void PhoneBook::SEARCH(){
 			std::cout<<j<<"         ";
 			std::cout<<" | ";
 			int k = 0;
-			while (this->contacts[j].first_name[k])
+			while (this->contacts[j].get_field(0)[k])
 			{
-				if (k == 9 && this->contacts[j].first_name[k+1])
+				if (k == 9 && this->contacts[j].get_field(0)[k+1])
 				{
 					std::cout<<".";
 					k++;
 					break;
 				}
 				else
-					std::cout<<this->contacts[j].first_name[k];
+					std::cout<<this->contacts[j].get_field(0)[k];
 				k++;
 			}
 			while (k < 10){
@@ -56,16 +84,16 @@ void PhoneBook::SEARCH(){
 			}
 			std::cout<<" | ";
 			k = 0;
-			while (this->contacts[j].last_name[k])
+			while (this->contacts[j].get_field(1)[k])
 			{
-				if (k == 9 && this->contacts[j].last_name[k+1])
+				if (k == 9 && this->contacts[j].get_field(1)[k+1])
 				{
 					std::cout<<".";
 					k++;
 					break;
 				}
 				else
-					std::cout<<this->contacts[j].last_name[k];
+					std::cout<<this->contacts[j].get_field(1)[k];
 				k++;
 			}
 			while (k < 10){
@@ -74,16 +102,16 @@ void PhoneBook::SEARCH(){
 			}
 			std::cout<<" | ";
 			k = 0;
-			while (this->contacts[j].nickname[k])
+			while (this->contacts[j].get_field(2)[k])
 			{
-				if (k == 9 && this->contacts[j].nickname[k+1])
+				if (k == 9 && this->contacts[j].get_field(2)[k+1])
 				{
 					std::cout<<".";
 					k++;
 					break;
 				}
 				else
-					std::cout<<this->contacts[j].nickname[k];
+					std::cout<<this->contacts[j].get_field(2)[k];
 				k++;
 			}
 			while (k < 10){
@@ -114,11 +142,11 @@ void PhoneBook::SEARCH(){
 		else
 		{
 			//display contact
-			std::cout <<"first_name = "<<this->contacts[i].first_name<<std::endl;
-			std::cout <<"last_name = "<<this->contacts[i].last_name<<std::endl;
-			std::cout <<"nickname = "<<this->contacts[i].nickname<<std::endl;
-			std::cout <<"phone_number = "<<this->contacts[i].phone_number<<std::endl;
-			std::cout <<"darkest_secret = "<<this->contacts[i].darkest_secret<<std::endl;
+			std::cout <<"first_name = "<<this->contacts[i].get_field(0)<<std::endl;
+			std::cout <<"last_name = "<<this->contacts[i].get_field(1)<<std::endl;
+			std::cout <<"nickname = "<<this->contacts[i].get_field(2)<<std::endl;
+			std::cout <<"phone_number = "<<this->contacts[i].get_field(3)<<std::endl;
+			std::cout <<"darkest_secret = "<<this->contacts[i].get_field(4)<<std::endl;
 		}
 	}
 }
