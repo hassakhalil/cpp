073a251bfcdce8a713a4ac2a6fd60c2df03190a5
Hassan Khalil <hkhalil@e2r3p4.1337.ma>
2022-12-03 20:15:28 +0100

    nothing

---
+++ cpp_module02/ex00/Fixed.cpp
@@ -0,0 +1,11 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(const Fixed& f)
+{
+    this->n = f.n;
+}
+
+Fixed& Fixed::operator= (const Fixed& f)
+{
+
+}
--- cpp_module02/ex00/Fixed.hpp
+++ cpp_module02/ex00/Fixed.hpp
@@ -3,10 +3,13 @@
 
 class Fixed {
     int n;
-    static const int m;
+    static const int m = 8;
     public:
-
+    Fixed(): n(0){}
+    Fixed(const Fixed& f);
+    Fixed& operator = (const Fixed& f);
+    ~Fixed();
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
 };
-
-
 #endif
