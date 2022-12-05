ee6b4fb46228f158fe74b8514a435f0b2c066d2a
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-05 16:24:27 +0100

    changed the copy constructor implementation

--- cpp_module02/ex00/Fixed.cpp
+++ cpp_module02/ex00/Fixed.cpp
@@ -6,18 +6,18 @@ Fixed::Fixed()
     std::cout<<"Default constructor called"<<std::endl;
 }
 
-Fixed::Fixed(const Fixed& f)
-{
-    std::cout<<"Copy constructor called"<<std::endl;
-    this->n = f.getRawBits();
-}
-
 Fixed& Fixed::operator= (const Fixed& f)
 {
     std::cout<<"Copy assignment operator called"<<std::endl;
     this->n = f.getRawBits();
     return *this;
 }
+Fixed::Fixed(const Fixed& f)
+{
+    std::cout<<"Copy constructor called"<<std::endl;
+    *this = f;
+}
+
 
 Fixed::~Fixed()
 {
