c45ed7e5cd827ee0097d876336fd93ab7c9194ce
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-05 16:15:52 +0100

    ex00 done

--- cpp_module02/ex00/Fixed.cpp
+++ cpp_module02/ex00/Fixed.cpp
@@ -1,11 +1,38 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed()
+{
+    this->n = 0;
+    std::cout<<"Default constructor called"<<std::endl;
+}
+
 Fixed::Fixed(const Fixed& f)
 {
-    this->n = f.n;
+    std::cout<<"Copy constructor called"<<std::endl;
+    this->n = f.getRawBits();
 }
 
 Fixed& Fixed::operator= (const Fixed& f)
 {
+    std::cout<<"Copy assignment operator called"<<std::endl;
+    this->n = f.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout<<"Destructor called"<<std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout<<"getRawBits member function called"<<std::endl;
+    return this->n;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->n = raw;
+    std::cout<<"setRawBits member function called"<<std::endl;
 
 }
--- cpp_module02/ex00/Fixed.hpp
+++ cpp_module02/ex00/Fixed.hpp
@@ -1,11 +1,13 @@
 #ifndef FIXED_H
 #define FIXED_H
 
+#include <iostream>
+
 class Fixed {
     int n;
     static const int m = 8;
     public:
-    Fixed(): n(0){}
+    Fixed();
     Fixed(const Fixed& f);
     Fixed& operator = (const Fixed& f);
     ~Fixed();
