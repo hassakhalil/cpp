b7b926850401a1a4ab6a5cd2c41dea0d20be85eb
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-06 16:52:26 +0100

    adding ex02

---
+++ cpp_module02/ex02/Fixed.cpp
@@ -0,0 +1,88 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    this->n = 0; 
+    this->type =0;
+    std::cout<<"Default constructor called"<<std::endl;
+}
+
+Fixed::Fixed(const int i)
+{
+    //convert the int to fixed
+    std::cout<<"Int constructor called"<<std::endl;
+    this->n = i << this->m;
+    this->type = 0;
+}
+
+Fixed::Fixed(const float f)
+{
+    //convert the float to fixed
+    std::cout<<"Float constructor called"<<std::endl;
+    float i = f;
+    float j = 256.0;
+    float k = i*j;
+    this->n = k;
+    this->type = 1;
+}
+
+Fixed& Fixed::operator= (const Fixed& f)
+{
+    std::cout<<"Copy assignment operator called"<<std::endl;
+    this->n = f.getRawBits();
+    this->type = f.getType();
+    return *this;
+}
+Fixed::Fixed(const Fixed& f)
+{
+    std::cout<<"Copy constructor called"<<std::endl;
+    *this = f;
+}
+
+
+Fixed::~Fixed()
+{
+    std::cout<<"Destructor called"<<std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    // std::cout<<"getRawBits member function called"<<std::endl;
+    return this->n;
+}
+
+int Fixed::getType(void) const
+{
+    return this->type;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->n = raw;
+    // std::cout<<"setRawBits member function called"<<std::endl;
+
+}
+
+float Fixed::toFloat( void ) const
+{
+    if (this->type == 0)
+        return this->n >>this->m;
+    float i = this->n;
+    float j = 256.0;
+    float k = i/j;
+    return k;
+}
+
+int Fixed::toInt( void ) const
+{
+    if (this->type == 1)
+        return this->toFloat();
+    else
+        return this->n >>this->m;
+}
+
+std::ostream& operator << (std::ostream& os, const Fixed& f)
+{
+    os<<f.toFloat();
+    return os;
+}
---
+++ cpp_module02/ex02/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+    int n;
+    static const int m = 8;
+    int type;
+    public:
+    Fixed();
+    Fixed(const int n);
+    Fixed(const float n);
+    Fixed(const Fixed& f);
+    Fixed& operator = (const Fixed& f);
+    ~Fixed();
+    int getRawBits(void) const;
+    int getType(void) const;
+    void setRawBits(int const raw);
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    // std::ostream& operator << (std::ostream& os);
+};
+
+std::ostream& operator << (std::ostream& os, const Fixed& f);
+
+#endif
