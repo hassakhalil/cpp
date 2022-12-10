9492f4d170cf68707a05b1c631944093496e3992
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-10 21:45:30 +0100

    adding module03 ex01

--- cpp_module03/ex00/ClapTrap.cpp
+++ cpp_module03/ex00/ClapTrap.cpp
@@ -1,6 +1,7 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(){
+    this->name = "Default_name";
     this->hit = 10;
     this->damage = 0;
     this->energy = 10;
@@ -15,6 +16,14 @@ ClapTrap::ClapTrap(std::string& name){
     std::cout<<"constuctor called"<<std::endl;
 }
 
+ClapTrap& ClapTrap::operator = (const ClapTrap& c){
+    std::cout<<"Copy assignment operator called"<<std::endl;    
+    this->name = c.name;
+    this->hit = c.hit;
+    this->damage = c.damage;
+    this->energy = c.energy;
+    return *this;
+}
 ClapTrap::ClapTrap(const ClapTrap& c){
     std::cout<<"Copy constuctor called"<<std::endl;
     this->name = c.name;
@@ -22,15 +31,20 @@ ClapTrap::ClapTrap(const ClapTrap& c){
     this->damage = c.damage;
     this->energy = c.energy;
 }
-ClapTrap& ClapTrap::operator = (const ClapTrap& c){
-    std::cout<<"Copy assignment operator called"<<std::endl;
-    *this(c);
-    return *this;
-}
 
 ClapTrap::~ClapTrap(){
-        std::cout<<"Destuctor called"<<std::endl;
+     std::cout<<"Destuctor called"<<std::endl;
 }
-void ClapTrap::attack(const std::string& target){}
-void ClapTrap::takeDamage(unsigned int amount){}
-void ClapTrap::beRepaired(unsigned int amount){}
+void ClapTrap::attack(const std::string& target){
+    this->energy--;
+    std::cout<<"ClapTrap "<<this->name<<" attacks "<<target<<", causing "<<this->damage<<" points of damage!"<<std::endl;
+}
+void ClapTrap::takeDamage(unsigned int amount){
+    std::cout<<"ClapTrap "<<this->name<<" took "<<amount<<" amount of damage"<<std::endl;
+    this->hit -= amount;
+}
+void ClapTrap::beRepaired(unsigned int amount){
+    this->hit += amount;
+    std::cout<<"ClapTrap "<<this->name<<" repaired + "<<amount<<std::endl;
+    this->energy--;
+}
--- cpp_module03/ex00/main.cpp
+++ cpp_module03/ex00/main.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
-    
+    ClapTrap a;
+    a.attack( "b");
 }
