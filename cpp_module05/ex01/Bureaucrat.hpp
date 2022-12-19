ca03ed5c93c9335e3753e6eff5a86e017e9df7cd
Hassan Khalil <hkhalil@e2r5p12.1337.ma>
2022-12-19 18:18:29 +0100

    adding ex01

---
+++ cpp_module05/ex01/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+class Bureaucrat{
+    protected:
+    const std::string name;
+    int grade;
+    public:
+    class GradeTooHighException : public std::exception{
+        public:
+        const char *what()const _NOEXCEPT{return "GradeTooHigh";} 
+    };
+    class GradeTooLowException : public std::exception{
+        public:
+        const char *what()const _NOEXCEPT{return  "GradeTooLow";} 
+    };
+    GradeTooHighException h;
+    GradeTooLowException l;
+    Bureaucrat();
+    Bureaucrat(int grade, std::string name);
+    Bureaucrat(const Bureaucrat& b);
+    Bureaucrat& operator = (const Bureaucrat& b);
+    ~Bureaucrat();
+    std::string GetName()const;
+    int GetGrade()const;
+    void IncrimentGrade(int i);
+    void DecrementGrade(int i);
+
+};
+
+std::ostream& operator << (std::ostream& os, const Bureaucrat& f);
+#endif
---
+++ cpp_module05/ex01/Form.hpp
@@ -0,0 +1,6 @@
+#ifndef FORM_H
+#define FORM_H
+
+
+class Form{};
+#endif
