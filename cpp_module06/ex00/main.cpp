75446da67879dace9633a3c3ca68341c7e9f2309
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-26 04:37:54 +0100

    update

--- cpp_module06/ex00/main.cpp
+++ cpp_module06/ex00/main.cpp
@@ -40,7 +40,6 @@ int main(int ac, char* av[]){
     if (s.size() == 1 && (s[0] > '9' || s[0] < '0'))
     {
       Char a(s);
-      std::cout<<"[ char ]-----------"<<std::endl;
       std::cout<<a;
     }
     else if (s == "nan")
@@ -62,17 +61,14 @@ int main(int ac, char* av[]){
     {
         s = s.substr(0, s.size()-1);
         Float c(s);
-        std::cout<<"[ float ]------------"<<std::endl;
         std::cout<<c;
     }
     else if (s.find('.', 0) != std::string::npos){
       Double e(s);
-      std::cout<<"[ double ]-----------"<<std::endl;
       std::cout<<e;
     }
     else{
       Int b(s);
-      std::cout<<"[ int ]------------"<<std::endl;
       std::cout<<b;
     }
     return 0;
