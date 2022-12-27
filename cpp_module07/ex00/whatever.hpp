20c06c0c73d1c2b6045b75aa908668176d122b61
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-27 19:15:42 +0100

    update ex00

--- cpp_module07/ex00/whatever.hpp
+++ cpp_module07/ex00/whatever.hpp
@@ -1,4 +1,28 @@
 #ifndef WHATEVER_H
 #define WHATEVER_H
 
+#include <iostream>
+
+template <typename T>
+T max(T x, T y){
+    if (x ==y)
+        return y;
+    else
+        return (x > y) ? x:y;
+}
+
+template <typename T>
+T min(T x, T y){
+    if (x ==y)
+        return y;
+    else
+        return (x > y) ? y:x;
+}
+
+template <typename T>
+void   swap(T x, T y){
+    T tmp = x;
+    x = y;
+    y = tmp;
+}
 #endif
