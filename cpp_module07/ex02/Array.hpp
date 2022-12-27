c681b66adbe5aade81aae61b2eabe462555897fa
Hassan Khalil <hkhalil@e2r2p5.1337.ma>
2022-12-28 00:16:37 +0100

    update array

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -1,7 +1,16 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-class array{
-    
+template <typename T>
+class Array{
+    //private array template
+    T* array;
+    int size;
+    public:
+    Array();
+    Array(unsigned int n);
+    Array& operator = (const Array& f);
+    //overload []
+    int size()const;
 };
 #endif
