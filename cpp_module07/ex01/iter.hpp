6c2147484e2cf12faee616f19392bc2971e07707
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-27 19:55:45 +0100

    adding ex01

--- cpp_module07/ex01/iter.hpp
+++ cpp_module07/ex01/iter.hpp
@@ -3,7 +3,11 @@
 
 #include <iostream>
 
-void iter(){
-    
+template<typename T, typename U, typename V>
+void iter(T add, U len, V fct){
+    for(U i = 0; i<len ; i++)
+    {
+        fct((*add)[i]);
+    }
 }
 #endif
--- cpp_module07/ex01/main.cpp
+++ cpp_module07/ex01/main.cpp
@@ -1,5 +1,20 @@
 #include "iter.hpp"
 
+void test0(char c){
+    std::cout <<c<<std::endl;
+}
+
+void test1(int i) {
+    i++;
+    std::cout<<i <<" ";
+}
+
 int main(){
-    
+    std::string s = "hello world!";
+    int arr[10] = {1,2,3,4,5,6,7,8,9};
+    std::cout<<"----------test0-----------"<<std::endl;
+    iter(&s, s.size(), test0);
+    std::cout<<"----------test1-----------"<<std::endl;
+    iter(&arr, 10, test1);
+    std::cout<<std::endl;    
 }
