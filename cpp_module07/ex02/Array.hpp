1135223ddf0c5c11d1681ea6ae1e6c7eb650c490
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-28 01:58:11 +0100

    added ex02

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -1,16 +1,62 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <iostream>
+
 template <typename T>
 class Array{
     //private array template
-    T* array;
-    int size;
+    T* arr;
+    int siz;
     public:
     Array();
     Array(unsigned int n);
     Array& operator = (const Array& f);
-    //overload []
+    T& operator [](int);
     int size()const;
 };
+
+template <typename T>
+int Array<T>::size()const{
+    return this->siz;
+}
+
+template <typename T>
+Array<T>::Array(){
+    std::cout<<"default constructor called"<<std::endl;
+    this->array = 0;
+    this->siz = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n){
+    std::cout<<"constructor called"<<std::endl;
+    this->siz = n;
+    this->arr = new T[n];
+    for (int i = 0; i < (int)n ; i++)
+    {
+        this->arr[i] = 0;
+    }
+}
+
+template <typename T>
+Array<T>& Array<T>::operator = (const Array<T>& f){
+
+    if (this->arr)
+        delete [] this->arr;
+    this->siz = f.siz;
+    this->arr = new T[this->siz];
+    return *this;
+}
+
+template <typename T>
+T& Array<T>::operator [](int index){
+    if (index >= this->siz || index < 0)
+    {
+        std::exception e;
+        throw e;
+    }
+    return this->arr[index];
+}
+
 #endif
--- cpp_module07/ex02/main.cpp
+++ cpp_module07/ex02/main.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+    try
+    {
+        Array<int> a(10);
+        std::cout<<a.size()<<std::endl;
+        int j = 0;
+        for (int i = 0;i<10;i++)
+        {
+            a[i] = j++;
+            std::cout<<"a["<<i<<"] == "<<a[i]<<std::endl;
+        }
+        a[j];
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << "invalid index" <<std::endl;
+    }
     
 }
