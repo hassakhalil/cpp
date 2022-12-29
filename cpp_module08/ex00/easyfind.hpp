0c5ad4375b477ad2e6446a6c4aaa16eb4f620a23
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-29 01:55:01 +0100

    module08 initial commit

---
+++ cpp_module08/ex00/easyfind.hpp
@@ -0,0 +1,10 @@
+#ifndef EASYFIND_H
+#define EASYFIND_H
+
+template<typename T, int>
+void easyfind(T cont, int n){
+//find the first occurrence of (int n) in (T cont)
+    //else->throw an exception or return an error
+}
+
+#endif
