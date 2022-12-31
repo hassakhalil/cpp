54093ca75ec44831cd61e5f56606ebe0a36763f7
hassan khalil <hassan@hassans-MacBook-Pro.local>
2022-12-31 02:30:39 +0100

    update ex02

--- cpp_module08/ex02/MutantStack.hpp
+++ cpp_module08/ex02/MutantStack.hpp
@@ -1,11 +1,16 @@
 #ifndef MUTANTSTACK_H
 #define MUTANTSTACK_H
+#include <iostream>
 #include <stack>
+#include <iterator>
 
-template<typename T>
+template <class T, class Container = std::vector<T> >
 
 class MutantStack :public std::stack<T>{
     //add iterator
+    class iterator{
+    
+    };
 
 };
 
