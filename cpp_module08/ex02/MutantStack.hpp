4c1f7b214c1af947ab15f59af0715d03d4b04d74
Hassan Khalil <hkhalil@e2r8p10.1337.ma>
2023-01-05 23:39:22 +0100

    update

--- cpp_module08/ex02/MutantStack.hpp
+++ cpp_module08/ex02/MutantStack.hpp
@@ -9,19 +9,16 @@ class MutantStack :public std::stack<T>{
     //add iterator
     //canonical form
     public:
-        // template <typename T>
-    class iterator: public std::iterator
-    {
- 
+    class iterator{
+        public:
+         iterator& operator++() {return ++MutantStack::c;}
+        iterator operator++(int) {return MutantStack::c++;}
+        bool operator==(iterator other) const {return *MutantStack::c == *other;}
+        bool operator!=(iterator other) const {return !(*MutantStack::c == *other);}
+        iterator& operator*() const {return *MutantStack::c;}
     };
-    // iterator begin() {return iterator(FROM);}
-    // iterator end() {return iterator(TO >= FROM? TO+1 : TO-1);}
-    // MutantStack();
-    // MutantStack(const MutantStack& s);
-    // MutantStack& operator = (const MutantStack& s);
-    // ~MutantStack();
-    std::iterator begin() { return this->c.begin(); }
-    std::iterator end() { return this->c.end(); }
+    iterator begin(){return this->c.begin();}
+    iterator end(){return this->c.end();}
 };
 
 
