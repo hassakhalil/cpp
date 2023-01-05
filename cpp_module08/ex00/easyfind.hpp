1da2ea31bbccfdedb6063031762a539e7d016320
hassan khalil <hassan@hassans-MacBook-Pro.local>
2023-01-05 21:59:33 +0100

    update

--- cpp_module08/ex00/easyfind.hpp
+++ cpp_module08/ex00/easyfind.hpp
@@ -5,8 +5,6 @@
 
 template<typename T>
 void easyfind(T cont, int n){
-//find the first occurrence of (int n) in (T cont)
-    //else->throw an exception or return an error
     if (find(cont.begin(), cont.end(), n) != cont.end())
         std::cout<<"Element found"<<std::endl;
     else
