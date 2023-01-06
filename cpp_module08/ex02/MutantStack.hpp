b237b5b3764681bea358fe82b99c0b2f549f9e33
hassan khalil <hassan@hassans-MacBook-Pro.local>
2023-01-06 01:46:48 +0100

    done

--- cpp_module08/ex02/MutantStack.hpp
+++ cpp_module08/ex02/MutantStack.hpp
@@ -6,20 +6,99 @@
 
 template <typename T>
 class MutantStack :public std::stack<T>{
-    //add iterator
-    //canonical form
     public:
+    MutantStack();
+    MutantStack(const MutantStack& m);
+    MutantStack& operator =(const MutantStack& m);
+    ~MutantStack();
     class iterator{
+        T *value;
         public:
-         iterator& operator++() {return ++MutantStack::c;}
-        iterator operator++(int) {return MutantStack::c++;}
-        bool operator==(iterator other) const {return *MutantStack::c == *other;}
-        bool operator!=(iterator other) const {return !(*MutantStack::c == *other);}
-        iterator& operator*() const {return *MutantStack::c;}
+        iterator();
+        iterator(const iterator& i);
+        iterator& operator =(const iterator& i);
+        ~iterator();
+        void setValue(T& val);
+        iterator& operator++();
+        iterator operator-- ();
+        bool operator==(iterator other) const;
+        bool operator!=(iterator other) const;
+        T operator*() const ;
     };
-    iterator begin(){return this->c.begin();}
-    iterator end(){return this->c.end();}
+    iterator begin();
+    iterator end();
 };
 
+template <typename T>
+MutantStack<T>::MutantStack(){
+}
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack& m){
+    *this = m;
+}
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T>& m){
+    *this = m;
+    return *this;
+}
+template <typename T>
+MutantStack<T>::~MutantStack(){
+}
 
+template <typename T>
+MutantStack<T>::iterator::iterator(){
+    this->value = 0;
+}
+template <typename T>
+MutantStack<T>::iterator::iterator(const MutantStack<T>::iterator& i){
+    this->value = i.value;
+}
+
+template <typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator =(const MutantStack<T>::iterator& i){
+    this->value = i.value;
+    return *this;
+}
+
+template <typename T>
+MutantStack<T>::iterator::~iterator(){
+
+}
+
+template <typename T>
+void MutantStack<T>::iterator::setValue(T& val){
+            this->value = &val;
+}
+template <typename T>
+
+typename MutantStack<T>::iterator& MutantStack<T>::iterator::operator++() {
+        ++this->value;
+        return *this;
+}
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::iterator::operator-- (){
+        --this->value;
+        return *this;
+}
+template <typename T>
+bool MutantStack<T>::iterator::operator==(MutantStack<T>::iterator other) const {
+    return this->value == other.value;
+}
+template <typename T>
+bool MutantStack<T>::iterator::operator!=(MutantStack<T>::iterator other) const {
+    return !(this->value == other.value);}
+template <typename T>
+T  MutantStack<T>::iterator::operator*() const {
+    return *this->value;}
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(){
+        iterator it;
+        it.setValue(*this->c.begin());
+        return it;}
+template<typename T>
+
+typename MutantStack<T>::iterator MutantStack<T>::end(){
+        iterator it;
+        it.setValue(*this->c.end());
+        return it;}
 #endif
--- cpp_module08/ex02/main.cpp
+++ cpp_module08/ex02/main.cpp
@@ -1,13 +1,15 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
+    std::cout<<"------------MutantStack output------------"<<std::endl;
 MutantStack<int> mstack;
 mstack.push(5);
 mstack.push(17);
 std::cout << mstack.top() << std::endl;
 mstack.pop();
-std::cout << mstack.size() << std::endl;
+std::cout<< mstack.size() << std::endl;
 mstack.push(3);
 mstack.push(5);
 mstack.push(737);
@@ -23,5 +25,26 @@ std::cout << *it << std::endl;
 ++it;
 }
 std::stack<int> s(mstack);
+std::cout<<"------------std::list output------------"<<std::endl;
+std::list<int> ls;
+ls.push_back(5);
+ls.push_back(17);
+std::cout << *ls.begin() << std::endl;
+ls.pop_back();
+std::cout<< ls.size() << std::endl;
+ls.push_back(3);
+ls.push_back(5);
+ls.push_back(737);
+//[...]
+ls.push_back(0);
+std::list<int>::iterator it2 = ls.begin();
+std::list<int>::iterator ite2 = ls.end();
+++it2;
+--it2;
+while (it2 != ite2)
+{
+std::cout << *it2 << std::endl;
+++it2;
+}
 return 0;
 }
