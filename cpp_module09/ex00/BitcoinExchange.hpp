cb21bac3e0f27267071a9a088af6a492edc79afe
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-03-15 15:25:33 +0100

    adding ex00

--- cpp_module09/ex00/BitcoinExchange.hpp
+++ cpp_module09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,73 @@
 #ifndef BITCIONEXCHANGE_HPP
 #define BITCIONEXCHANGE_HPP
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <utility>
 
+class BitcoinExchange{
+    public:
+        BitcoinExchange(){
 
+            std::ifstream file;
+            std::string date;
+            //debug
+            std::cout<<"default constructor"<<std::endl;
+            //end debug
+            std::string value_str;
+            std::stringstream ss;
+            float value;
+            file.open("data.csv");
+            if (!file)
+            {
+                std::cout<<"Error: could not open data base file"<<std::endl;
+                return;
+            }
+                //debug
+                std::cout<<"before loop"<<std::endl;
+                //end debug 
+            int i = 0;
+            while (!std::getline(file, date, ',').eof())
+            {
+                //debug
+                // std::cout<<"inside loop"<<std::endl;
+                //end debug 
+                std::getline(file, value_str);
+                ss.str() = value_str; 
+                ss >> value;
+                _data.insert(std::pair<std::string,float>(date,value));
+                i++;
+            }
+            //read line by line
+            //insert in map
+            file.close();
+            //debug
+            std::cout<<"i = "<<i<<std::endl;
+            //end debug
+        }
+        ~BitcoinExchange(){
+            //debug
+            std::cout<<"destructor"<<std::endl;
+            //end debug
+        }
+        BitcoinExchange(const BitcoinExchange& src){
+            //debug
+            std::cout<<"copy constructor"<<std::endl;
+            //end debug
+            this->_data = src._data;
+        }
+        BitcoinExchange & operator=(BitcoinExchange const & other){
+            //debug
+            std::cout<<"copy assignment operator overload"<<std::endl;
+            //end debug
+
+            _data = other._data;
+            return *this;
+        }
+    private:
+    std::map<std::string,float> _data;
+};
 
 #endif
--- cpp_module09/ex00/main.cpp
+++ cpp_module09/ex00/main.cpp
@@ -5,14 +5,18 @@
 int main(int ac, char **av){
     //check if file exists
     //check if file is valid (date format / price format) line by line
+    //check if database file exists and valid
     //convert
     std::ifstream file;
     file.open(av[1]);
     if (ac != 2 || !file)
     {
-        std::cout<<"Error: could not open file"<<std::endl;
+        std::cout<<"Error: could not open file."<<std::endl;
         return 1;
     }
-
+    //debug
+    std::cout<<"filename = "<<av[1]<<std::endl;
+    //end debug
+    BitcoinExchange b;
 }
 
